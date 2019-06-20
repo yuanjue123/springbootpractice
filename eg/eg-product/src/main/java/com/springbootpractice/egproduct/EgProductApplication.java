875aa7fa174cb2079b8800f57e947835615a57c3
lifuchun <fuchun.li@lifesense.com>
2019-06-20 17:42:08 +0800

    熔断

--- eg/eg-product/src/main/java/com/springbootpractice/egproduct/EgProductApplication.java
+++ eg/eg-product/src/main/java/com/springbootpractice/egproduct/EgProductApplication.java
@@ -2,8 +2,12 @@ package com.springbootpractice.egproduct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.springbootpractice.api.user")
+@EnableCircuitBreaker
 public class EgProductApplication {
 
     public static void main(String[] args) {
