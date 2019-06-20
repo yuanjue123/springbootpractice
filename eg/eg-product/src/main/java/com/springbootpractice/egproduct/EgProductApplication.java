53ba02f713d2e32d4fbe8b2da2e8fcbdfb3cec73
lifuchun <fuchun.li@lifesense.com>
2019-06-20 16:05:55 +0800

    product

---
+++ eg/eg-product/src/main/java/com/springbootpractice/egproduct/EgProductApplication.java
@@ -0,0 +1,13 @@
+package com.springbootpractice.egproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgProductApplication.class, args);
+    }
+
+}
