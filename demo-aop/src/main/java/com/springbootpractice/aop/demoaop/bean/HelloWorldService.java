0461509e9a23de5524711ace23f6017b950d0afb
李福春 <fuchun.li@lifesense.com>
2020-01-02 11:10:33 +0800

    apodemo

---
+++ demo-aop/src/main/java/com/springbootpractice/aop/demoaop/bean/HelloWorldService.java
@@ -0,0 +1,23 @@
+package com.springbootpractice.aop.demoaop.bean;
+
+import java.util.Objects;
+
+/**
+ * 说明：TODO
+ * @author carter
+ * 创建时间： 2020年01月02日 9:42 上午
+ **/
+
+public class HelloWorldService implements IHelloWorld {
+    /**
+     * 问候
+     * @param name 姓名
+     */
+    @Override
+    public void sayHello(String name) {
+        if (Objects.isNull(name) || Objects.equals("", name)) {
+            throw new RuntimeException("name为空");
+        }
+        System.out.println("hello  " + name);
+    }
+}
